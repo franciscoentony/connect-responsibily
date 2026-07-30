ac0f6c4217ee6c02e7c7d63f57538cdf4532d2aa
Francisco Entony <franciscoentonydev@gmail.com>
2026-07-30 22:38:04 +0000

    atualizando `build_files.sh`

--- build_files.sh
+++ build_files.sh
@@ -1,4 +1,4 @@
 echo " Building the project..."
-python3 -m pip install -r requirements.txt
+python3 -m pip install --system -r requirements.txt
 python3 manage.py collectstatic --noinput --clear
 echo " Build Configuration Completed!"
